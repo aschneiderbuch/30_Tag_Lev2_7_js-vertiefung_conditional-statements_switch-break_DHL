1d2f1975da150dc3b227a8b5f6fba75a33106f9b
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-24 20:19:49 +0100

    frage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
 console.log("test");
+
+/* 
+
+Frage
+
+value von selectbox holen
+
+in Variable speichern
+
+
+Funktion showtxt()
+
+    switch case break default=(Extra große Größe)
+
+
+output in html
+
+*/
